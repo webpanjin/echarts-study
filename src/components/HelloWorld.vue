8a8f6e9cb91a823c3ade19045118c844b522d48e
webpanjin <2697857844@qq.com>
2019-04-05 14:07:41 +0800

    测试

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+<div id="myChart" ref="myChart"></div> 
+</template>
+<style>
+#myChart {
+width: 95%;
+height: 300px;
+margin: 20px auto;
+border: 1px solid #CCC
+}
+</style>
+<script>
+export default {}
+</script>
